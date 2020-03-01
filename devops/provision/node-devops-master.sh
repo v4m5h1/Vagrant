30009ac6267152160112b02237302b918303786a
sudheer veeravalli <veersudhir83@gmail.com>
2020-03-01 07:37:49 +0530

    Adjusted jenkins and ansible installation scripts in devops

--- devops/provision/node-devops-master.sh
+++ devops/provision/node-devops-master.sh
@@ -8,13 +8,18 @@ add-apt-repository -y ppa:openjdk-r/ppa && apt-get update && apt-get install -y 
 java -version
 
 echo "Installing Jenkins"
-wget --no-check-certificate https://pkg.jenkins.io/debian-stable/binary/jenkins_2.150.3_all.deb
-dpkg -i jenkins_2.150.3_all.deb
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+echo 'deb https://pkg.jenkins.io/debian-stable binary/' >> /etc/apt/sources.list
+apt-get update
+apt-get install jenkins
 # If you need to run jenkins on different port other than 8080, use the below command
 # sudo sed 's/HTTP_PORT=8080/HTTP_PORT=9999/g' /etc/default/jenkins
-service jenkins restart
+# service jenkins restart
 
 echo "Installing Ansible"
+apt-get update
+apt-get install software-properties-common
+apt-add-repository --yes --update ppa:ansible/ansible
 apt-get install -y ansible
 ansible --version
 
