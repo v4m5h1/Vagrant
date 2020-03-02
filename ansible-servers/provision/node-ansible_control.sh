99741a6aeadfd59247dbd2e6b6061fe269908727
sudheer veeravalli <veersudhir83@gmail.com>
2020-03-02 07:27:22 +0530

    Adding provision for ansible control

---
+++ ansible-servers/provision/node-ansible_control.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+## Provisioning necessary softwares on the Virtual Machine
+echo "Installing JDK"
+apt-get update
+apt-get install -y openjdk-8-jre-headless
+java -version
+
+echo "Installing Jenkins"
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+echo 'deb https://pkg.jenkins.io/debian-stable binary/' >> /etc/apt/sources.list
+apt-get update
+apt-get install -y jenkins
+# If you need to run jenkins on different port other than 8080, use the below command
+# sudo sed 's/HTTP_PORT=8080/HTTP_PORT=9999/g' /etc/default/jenkins
+# service jenkins restart
+
+echo "Installing Ansible"
+apt-get update
+apt-get install software-properties-common
+apt-add-repository --yes --update ppa:ansible/ansible
+apt-get install -y ansible
+ansible --version
+
+# optional tools for learning 
+apt-get install -y -q ipvsadm tree
+# lsns is helpful from util-linux, this is installed already
