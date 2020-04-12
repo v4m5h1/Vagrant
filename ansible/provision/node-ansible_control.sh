612dee42a1513b1dd69522de213c539d054dd055
Sudheer Veeravalli <veersudhir83@gmail.com>
2020-04-12 21:51:03 +0530

    provisioning docker as well

--- ansible/provision/node-ansible_control.sh
+++ ansible/provision/node-ansible_control.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-
 ## Provisioning necessary softwares on the Virtual Machine
+
+# Installing JDK
 echo "Installing JDK"
 apt-get update
 apt-get install -y openjdk-8-jdk
 java -version
 
+# Installing Jenkins
 echo "Installing Jenkins"
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 echo 'deb https://pkg.jenkins.io/debian-stable binary/' >> /etc/apt/sources.list
@@ -16,6 +18,7 @@ apt-get install -y jenkins
 # sudo sed 's/HTTP_PORT=8080/HTTP_PORT=9999/g' /etc/default/jenkins
 # service jenkins restart
 
+# Installing Ansible
 echo "Installing Ansible"
 apt-get update
 apt-get install software-properties-common
@@ -23,6 +26,12 @@ apt-add-repository --yes --update ppa:ansible/ansible
 apt-get install -y ansible
 ansible --version
 
+# Installing Docker
+echo "Installing Docker"
+apt-get install -y docker.io
+systemctl start docker
+usermod -AG docker ${USER}
+docker --version
+
 # optional tools for learning 
 apt-get install -y -q ipvsadm tree net-tools
-
